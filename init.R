e576c61a1bc3e6f3ff708b7122ece6d24574175b
ntorenduwayo <jpnduwntore@gmail.com>
2022-07-08 09:41:39 -0700

    Updating init.R file'

--- init.R
+++ init.R
@@ -3,7 +3,11 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("modeldata", "plotly")
+my_packages = c("modeldata", "plotly", "shinydashboard", "shinyWidgets",
+                "maps", "dplyr", "leaflet", "shinycssloaders", "shinythemes",
+                "rio", "DT", "stargazer", "readr", "purrr", "tidyr", "ggplot2",
+                "psych", "cowplot", "AICcmodavg", "tidyverse", "moderndive",
+                "plot3D", "misc3d", "recipes", "shinythemes", "data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {

d3242889c3afd71732378afeac4d0845abde2f56
ntorenduwayo <jpnduwntore@gmail.com>
2022-07-08 12:45:35 -0700

    Updating

--- init.R
+++ init.R
@@ -3,10 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("modeldata", "plotly", "shinydashboard","shiny",
-                "maps", "dplyr", "readr", "purrr", "ggplot2",
-                "psych", "cowplot", "AICcmodavg", "tidyverse", "moderndive",
-                "recipes", "data.table")
+my_packages = c("modeldata", "plotly", "shinydashboard", "data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
